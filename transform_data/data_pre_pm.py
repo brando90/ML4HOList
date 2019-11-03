c81dd3d0cef35c57fd3dc5f8d54f0aba6bfd8e4b
MeiluYuan <AbyssSelf@outlook.com>
2019-11-03 01:08:41 -0500

    Update data_pre_pm.py
    prepare the tactic prediction data and also acquire some goal-tactic pair statistics ...

--- transform_data/data_pre_pm.py
+++ transform_data/data_pre_pm.py
@@ -1,118 +1,102 @@
 """
-To prepare the data for basic classification of the project ...
-Extract the goal-tactic pairs ...
-# What files we should have: txt? csv? json ??? We'd better to have a json file ???
+# prepare the training data for premise slection and then write them as csv files ...
+# some data statistics about the HOList Dataset ...
+# Maybe it's better for us to make some sample csv file to open and review conveniently ...
 """
 
 import os 
+import sys 
 import re
 import csv
 import glob
 import numpy as np 
+from collections import namedtuple     # nametuple ???
 
-pm_add = '/Users/meiluyuan/Desktop/Theorem_Proving/data/premise_selection/'
+csv.field_size_limit(sys.maxsize)
+# It's better to combine the two sets together ...
+pm_add = '/Users/meiluyuan/Desktop/Theorem_Proving/data/premise_selection/human_test/'
 
-# read the whole file by line (per line represents a theorem proof ...)
-def rw_one_file(file):
-    with open(file, 'r') as f:
-        for line in f:
-            print ("The first data in file {} is :\n".format(file))
-            print (line) 
-
-
-# read and write the data woith appropriate way ...
-def try_extract():
-    all_files = glob.glob('/Users/meiluyuan/deephol-data/deepmath/deephol/proofs/human/test/*.pbtxt')
-    file = '/Users/meiluyuan/deephol-data/deepmath/deephol/proofs/human/test/prooflogs-00000-of-00537.pbtxt'
-    count = 0
-    count_other = 0     # this part is used to make the 
-    with open(file, 'r') as f:
+def get_stats (csv_file):
+    
+    with open(csv_file, 'r') as csv_f:
+        ### sample number and tactic category number and list ... ###
+        reader = csv.reader(csv_f)
+        next(reader)
         
-        # Here, we need to locate precisely ... Make sure we re the precise pattern ...
-        all_lines = f.readlines()
-        a_line = all_lines[0]
-        print ("This is a_line: {}".format(a_line))
-        print ("\n")
-        all_nodes = re.findall(r'nodes {   (.*?) status: PROVED }', a_line)    # with two bracelets outside the two  
-        print ("There are all_nodes: {}".format(all_nodes))
-        print ("\n")
-        # make a doc for dis-matched goal and proof number  
-        for node in all_nodes:
-            # how to get the matched result as expectecd ...
-            print (node)
-            all_goals = re.findall(r'goal {     conclusion: "(.*?)"     tag:', node)   # find all_goals or all_proofs ...
-            print ("all_goals are: {}".format(all_goals))
-            print ("\n")
-            all_tactics = re.findall(r'proofs {     tactic: "(.*?)"     result', node)
-            print ("all_proofs are: {}".format(all_tactics) )
-            # until now, we are able to extract all the data for premise slection, what kind of method we need to use for data organization ...
+        all_tactics = [ pair[1] for pair in reader ]
+        print ("The sample number of the dataset is {}".format( len(all_tactics) ))
+        tactic_category = set(all_tactics)
+        print ( "All the tactic categories are {}".format(len(tactic_category)) )
+        print ("They are: {}".format(tactic_category)) 
+
+    with open(csv_file, 'r') as csv_f:
+        ### the stats about the goal statement here include the max&min length,  ###
+        reader = csv.reader(csv_f)
+        next(reader)
+
+        def pure_goal(ori_data):
+            pure_goal = re.findall(r'"(.*?)"', ori_data)   # we need to input this pure data in our interactive program ...
+            return pure_goal
+        all_goals = [ pure_goal(pair[0]) for pair in reader ] 
+        all_lengths = [ len( str(goal)) for goal in all_goals ]
+        all_lengths = sorted(all_lengths)
+        total_num = len(all_lengths)
+        median = all_lengths[int(total_num/2)] 
+        print ("This is the median-length goal:\n {}".format(median))
+        print(all_goals[median])
+        print("the median character number of goal statement is {}".format(median))
+        lengths = np.array(all_lengths)
+        max_len = np.max(lengths)
+        min_len = np.min(lengths)
+        average_len = np.average(lengths)
+        print ("Goal stas of the training set are: \n max_length is {};\n min_length is {};\n median:{}\n average:{};\n".format(
+            max_len, min_len, median, average_len)) 
 
 
-
-# Try to make them more efficient with some mechanism ... 
-# the difference between the goal and proofs ...   # perhaps 
+# review the data organization and its structure again for further writing ...
+# also obtain the node info ...
 def try_main():
-    all_files = glob.glob('/Users/meiluyuan/deephol-data/deepmath/deephol/proofs/human/test/*.pbtxt')
-    for file in all_files:    # here I use [0] to limit the data for test ...
+    all_files = glob.glob('/Users/meiluyuan/deephol-data/deepmath/deephol/proofs/human/train/*.pbtxt')
+    pm = open(pm_add + 'train' + '.csv', 'w+')    # without marks include assumption and hypothesis ...
+    headers = ['goal', 'tactic']   
+    pm_csv = csv.writer(pm)
+    pm_csv.writerow(headers)
+    nodes_nums = []
+    for file in all_files:    # to test the results ...
         print (file)
         with open(file, 'r') as f:
             all_lines = f.readlines()
-            """ Here, we write the all the goal-tactic paris into a pbtxt file ... (refer the pytorch framework ...) """
-            pm_name = re.findall(r'/prooflogs(.*?).pbtxt', file)
-            print ("Current pm_name is {}".format(pm_name) )
-            pm = open(pm_add + 'goal_tactic' + pm_name[0] + '.pbtxt', 'w+')
             count_1 = 0
             for a_line in all_lines:   
                 all_nodes = re.findall(r'nodes {   (.*?) status: PROVED }', a_line)
-                count_2 = 0
+                nodes_nums.append(len(all_nodes))
+                count_2 = 0   # this is used to get the statistics of node number ...
                 for node in all_nodes:
-                    all_goals = re.findall(r'goal {(.*?)     tag:', node)
-                    """
-                    all_subgoals = re.findall(r'subgoals {(.*?)     tag:', node)
-                    all_goals = all_goals + all_subgoals
-                    """
+                    all_goals = re.findall(r'goal {(.*?)     tag:', node)   # two conditions?: hypothesis and assumption ...??? differences ... ???
                     all_tactics = re.findall(r'proofs {     tactic: "(.*?)"', node)
                     goal_num = len(all_goals)
                     tactic_num = len(all_tactics)
-                    """
-                    print ("Node is {}".format(node))
-                    print (all_goals)
-                    print (all_tactics)
-                    print ("goal_num is {}".format(goal_num) )
-                    print ("tactic_num is {}".format(tactic_num) )
-                    """
-                    # if the number of goals and tactics don't match each other, then we won't write it into our premise selection data ...
                     if goal_num == tactic_num:
                         for i in range(goal_num):
-                            pm.write('goal:' + all_goals[i] + ';')
-                            pm.write('tactic:' + all_tactics[i] + ';')
-                            pm.write('\n')
+                            # if it's set, we need to use set format ...
+                            goal = re.findall(r'"(.*?)"', all_goals[i])
+                            goal_tactic = [ goal, all_tactics[i] ]   
+                            pm_csv.writerow(goal_tactic)
+                    # whether we have this situation exists ... ?
                     elif goal_num - tactic_num == 1:
                         print ("File: {}, Line: {} Node Count: {} --- goal tactic don't match ...".format(file, count_1, count_2))
                         print ("Goal_num is {} and Tactic_num is {}, the difference is 1.".format(goal_num, tactic_num))
-                        for i in range(tactic_num):
-                            pm.write('goal:' + all_goals[i] + ';')
-                            pm.write('tactic:' + all_tactics[i] + ';')
-                            pm.write('\n')
                     else:
                         print ("File: {}, Line: {} Node Count: {} --- goal tactic don't match ...".format(file, count_1, count_2))
                         print ("Node is {}".format(node))
-                        print ('\n')
-                        print (all_goals)
-                        print ('\n')
-                        print (all_tactics)
-                        print ('\n')
                         print ("goal_num is {}".format(goal_num) )
                         print ("tactic_num is {}".format(tactic_num) )
                     count_2 = count_2 + 1
                 count_1 = count_1 + 1
-            pm.close() 
-         
-
-# look at pytorch support choose a file format to store the goal-tactic pair data 
-
-                
-
+    pm.close() 
+                    
 
 if __name__ == '__main__':
     try_main()
+    csv_file = pm_add + 'goal_tactic.csv'
+    get_stats(csv_file)
